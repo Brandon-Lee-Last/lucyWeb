274a1a88c1f8b517ab4b57c63ae4f0a22106d25a
Brandon <brandonl@runninghill.co.za>
2022-04-08 18:21:39 +0200

    ADD ALL FIXED

--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -9,21 +9,22 @@ const Projects = (props) => {
         history(`/project?id=${id}`);
     }
 
-    const imageStyles = {
-    backgroundPosition: 'center center',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundImage: "url('https://animecorner.me/wp-content/uploads/2022/03/sky-animation.png')"
-    }
-
     return(
    <div className="main-p">
     <h1 className="project-title"><spann style={{color: '#ffa680'}}>My</spann> Projects</h1>
     <div id="projects" className="project-container">
     <main className="project">
             {props.projectData.data.map((project, index) => {
+
+
+
                 return(
-                        <div key={index} onClick={() => getRedirected(project.id)} className="card" style={imageStyles}> 
+                        <div key={index} onClick={() => getRedirected(project.id)} className="card" style={{
+                            backgroundPosition: 'center center',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'cover',
+                            backgroundImage: `url('${project.imageUrl}')`
+                        }}> 
                         <h2 className="project-title">{project.title}</h2>
                         <h3 className="project-subtitle">view more details</h3>
                 </div>
